0f652b1c002075e8c9af73697a875ecf4b049094
Maiko Yasukochi <key.11.maico@gmail.com>
2024-11-21 07:39:57 +0900

    assignment 3

---
+++ src/main/java/maikoyasukochi_project/HelloJava.java
@@ -0,0 +1,10 @@
+package maikoyasukochi_project;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, Java");		
+	}
+
+}
---
+++ src/main/java/maikoyasukochi_project/Item.java
@@ -0,0 +1,19 @@
+package maikoyasukochi_project;
+
+public class Item {
+	
+	public String name;
+	public int price;
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Item item = new Item();
+		
+		item.name = "item A";
+		item.price = 100;
+
+		System.out.println(item.name + ": " + item.price + "yen");
+		
+	}
+
+}
